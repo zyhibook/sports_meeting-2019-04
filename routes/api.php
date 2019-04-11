e09540c681af42bab07cb2742a42dd0f9d7287df
XuZhixuan <sxxuzhixuan@hotmail.com>
2019-04-11 14:19:55 +0800

    添加接口，忽略锁文件

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('news', 'Api\DataController@news');
 Route::get('teams', 'Api\DataController@teams');
+Route::get('games', 'Api\DataController@games');
